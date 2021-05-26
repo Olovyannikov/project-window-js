ea5ddb7b77a256234543034e11c934939400a1c3
Ilya <olovyannikov_barista@icloud.com>
2021-05-26 17:46:30 +0300

    Add fix for modals

--- dist/script.js
+++ dist/script.js
@@ -15393,6 +15393,7 @@ const modals = () => {
     const modal = document.querySelector(modalSelector);
     const close = document.querySelector(closeSelector);
     const windows = document.querySelectorAll('[data-modal]');
+    const scroll = calcScroll();
     trigger.forEach(item => {
       item.addEventListener('click', e => {
         if (e.target) {
@@ -15403,7 +15404,8 @@ const modals = () => {
           window.style.display = 'none';
         });
         modal.style.display = 'block';
-        document.body.style.overflow = 'hidden'; // document.body.classList.add('modal-open');
+        document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scroll}px`; // document.body.classList.add('modal-open');
       });
     });
     close.addEventListener('click', () => {
@@ -15411,7 +15413,8 @@ const modals = () => {
         window.style.display = 'none';
       });
       modal.style.display = 'none';
-      document.body.style.overflow = ''; // document.body.classList.remove('modal-open');
+      document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`; // document.body.classList.remove('modal-open');
     });
     modal.addEventListener('click', e => {
       if (e.target === modal && closeClickOverlay) {
@@ -15419,7 +15422,8 @@ const modals = () => {
           window.style.display = 'none';
         });
         modal.style.display = 'none';
-        document.body.style.overflow = ''; // document.body.classList.remove('modal-open');
+        document.body.style.overflow = '';
+        document.body.style.marginRight = `0px`; // document.body.classList.remove('modal-open');
       }
     });
   }
@@ -15431,11 +15435,24 @@ const modals = () => {
     }, time);
   }
 
+  function calcScroll() {
+    let div = document.createElement('div');
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    return scrollWidth;
+  }
+
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
   bindModal('.phone_link', '.popup', '.popup .popup_close');
   bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
   bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_close', false);
-  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false); // showModalByTime('.popup', 60000);
+  bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
+  showModalByTime('.popup', 60000);
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (modals);
